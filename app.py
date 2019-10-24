c2be603ca78af7c73d8246bf0e84f982b86fb57e
Sukh Raj Limbu <limburaj65@gmail.com>
2019-10-25 03:21:24 +0530

    adding functionalities to flask app.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
@@ -13,6 +15,28 @@ def sms_reply():
     # Fetch the message
     msg = request.form.get('Body')
 
+    page = requests.get('https://www.linkedin.com/jobs/search/?keywords=intern&location=Bengaluru%2C%20Karnataka%2C%20India')
+    soup = BeautifulSoup(page.content, features="lxml")
+    openings = soup.find_all("a", class_="result-card__full-card-link")
+    if len(openings) > 10:
+        openings = openings[:10]
+
+    for x in openings:
+        links = x["href"]
+        openings_page = requests.get(links)
+        openings_soup = BeautifulSoup(openings_page.content, features="lxml")
+        job_title = openings_soup.find("h1", class_="topcard__title").text
+        job_location = openings_soup.find("span", class_="topcard__flavor topcard__flavor--bullet").text
+        company_title = openings_soup.find("a", class_="topcard__org-name-link").text
+        job_description = openings_soup.find("div", class_="description__text description__text--rich")
+        print(job_title, ', ', company_title, ', ', job_location)
+        apply_link = openings_soup.find("a", class_="apply-button apply-button--link")
+        if apply_link:
+            apply_link = apply_link["href"]
+        print(apply_link)
+        # print("Description:\n", job_description.text)
+        print("--------------------------------------------------------------------------")
+    
     # Create reply
     resp = MessagingResponse()
     resp.message("You said: {}".format(msg))
